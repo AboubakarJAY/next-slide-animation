d1cb2d5b2dff9f991dc7ade073be24ea75f6c0fb
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-11 17:06:43 +0100

    11 feb.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 "use strict";
 const btn = document.getElementById("btn");
 const texteBtn = document.getElementById("texte-btn");
+const imageEl = document.getElementById("imageEl");
+const iconChanger = () => {
+    const interval = setInterval(() => {
+        imageEl.src = "../public/right.png";
+        setTimeout(() => {
+            imageEl.src = "../public/right2.png";
+        }, 100);
+    }, 300);
+    setInterval(() => {
+        clearInterval(interval);
+    }, 2700);
+};
 if (btn && texteBtn) {
     btn.addEventListener("mouseenter", () => {
         texteBtn.style.display = "block";
         setTimeout(() => {
             btn.classList.add("active");
         }, 300);
+        iconChanger();
         setTimeout(() => {
             btn.classList.remove("active");
         }, 2700);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 const btn = document.getElementById("btn") as HTMLElement;
 const texteBtn = document.getElementById("texte-btn") as HTMLElement;
+const imageEl = document.getElementById("imageEl") as HTMLImageElement;
+
+const iconChanger = () => {
+  const interval = setInterval(() => {
+    imageEl.src = "../public/right.png";
+    setTimeout(() => {
+      imageEl.src = "../public/right2.png";
+    }, 100);
+  }, 300);
+  setInterval(() => {
+    clearInterval(interval);
+  }, 2700);
+};
 
 if (btn && texteBtn) {
   btn.addEventListener("mouseenter", () => {
@@ -7,6 +20,7 @@ if (btn && texteBtn) {
     setTimeout(() => {
       btn.classList.add("active");
     }, 300);
+    iconChanger();
     setTimeout(() => {
       btn.classList.remove("active");
     }, 2700);
