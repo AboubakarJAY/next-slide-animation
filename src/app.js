e1d2003818bfa8cd7d911d33b3f8603e58c689ce
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-11 01:28:35 +0100

    11 feb.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@
 const btn = document.getElementById("btn");
 const texteBtn = document.getElementById("texte-btn");
 if (btn && texteBtn) {
-    btn.addEventListener("mouseenter", function elastic() {
+    btn.addEventListener("mouseenter", () => {
         texteBtn.style.display = "block";
+        setTimeout(() => {
+            btn.classList.add("active");
+        }, 300);
+        setTimeout(() => {
+            btn.classList.remove("active");
+        }, 2700);
     });
     btn.addEventListener("mouseleave", () => {
         texteBtn.style.display = "none";
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,14 @@ const btn = document.getElementById("btn") as HTMLElement;
 const texteBtn = document.getElementById("texte-btn") as HTMLElement;
 
 if (btn && texteBtn) {
-  btn.addEventListener("mouseenter", function elastic() {
+  btn.addEventListener("mouseenter", () => {
     texteBtn.style.display = "block";
+    setTimeout(() => {
+      btn.classList.add("active");
+    }, 300);
+    setTimeout(() => {
+      btn.classList.remove("active");
+    }, 2700);
   });
 
   btn.addEventListener("mouseleave", () => {
